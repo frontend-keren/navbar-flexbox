ef0bd5ed0f7acb8496a280b1cc743360e257e406
konstantin <Rhfrfpz,hf1301>
2016-02-03 18:32:54 +0200

    end day

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
+
+    var divider = '<div class="flex row">' +
+        '<div style="border-bottom: 15px solid #fad280" class="box"></div>' +
+        '<div style="border-bottom: 15px solid #bfe79e" class="box"></div>' +
+        '<div style="border-bottom: 15px solid #fc9f66" class="box"></div>' +
+        '<div style="border-bottom: 15px solid #48d1cc" class="box"></div>' +
+        '</div>';
+
+    $('div.divider').append(divider);
+
     $('a.dropdown-toggle').each(function(){
         $(this).click(function(){
             $(this).next().toggleClass('on');
